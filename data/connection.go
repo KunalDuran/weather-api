de9830c3d444a55850c23ffc6265a3e82d0558c6
Kunal Duran <kunalduran13@gmail.com>
2023-07-30 12:08:07 +0530

    DB init updates

--- data/connection.go
+++ data/connection.go
@@ -28,13 +28,16 @@ func InitDB(host, port, user, password, dbName string) (Db *sql.DB, err error) {
 		return nil, err
 	}
 
-	if !tableExists(Db, dbName, "your_table_name") {
-		if err := createTable(Db); err != nil {
-			return nil, err
+	tableNames := []string{"users", "weather_history"}
+	for _, tableName := range tableNames {
+		if !tableExists(Db, dbName, tableName) {
+			if err := createTable(Db, tableName); err != nil {
+				return nil, err
+			}
 		}
 	}
 
-	fmt.Println("Database and table are ready.")
+	fmt.Println("Connected to database")
 	return Db, nil
 }
 
@@ -63,15 +66,57 @@ func tableExists(db *sql.DB, dbName, tableName string) bool {
 	return exists
 }
 
-func createTable(db *sql.DB) error {
-	query := `
-		CREATE TABLE your_table_name (
-			id INT AUTO_INCREMENT PRIMARY KEY,
-			column_name_1 datatype_1,
-			column_name_2 datatype_2,
-			...
-		)
-	`
+func createTable(db *sql.DB, tableName string) error {
+
+	var query string
+	switch tableName {
+	case "users":
+		query = `
+		CREATE TABLE users (
+			id INT PRIMARY KEY AUTO_INCREMENT,
+			username VARCHAR(255) UNIQUE NOT NULL,
+			password VARCHAR(255) NOT NULL,
+			date_of_birth DATE NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		) ENGINE=InnoDB;
+		`
+	case "weather_history":
+		query = `
+		CREATE TABLE weather_history (
+		  id INT NOT NULL AUTO_INCREMENT,
+		  city_name VARCHAR(255) NOT NULL,
+		  user_id INT NOT NULL,
+		  coord_lon FLOAT NOT NULL,
+		  coord_lat FLOAT NOT NULL,
+		  weather_id INT NOT NULL,
+		  weather_main VARCHAR(255) NOT NULL,
+		  weather_description VARCHAR(255) NOT NULL,
+		  weather_icon VARCHAR(255) NOT NULL,
+		  base VARCHAR(255) NOT NULL,
+		  temp FLOAT NOT NULL,
+		  feels_like FLOAT NOT NULL,
+		  temp_min FLOAT NOT NULL,
+		  temp_max FLOAT NOT NULL,
+		  pressure INT NOT NULL,
+		  humidity INT NOT NULL,
+		  visibility INT NOT NULL,
+		  wind_speed FLOAT NOT NULL,
+		  wind_deg INT NOT NULL,
+		  clouds_all INT NOT NULL,
+		  dt INT NOT NULL,
+		  sys_type INT NOT NULL,
+		  sys_id INT NOT NULL,
+		  sys_country VARCHAR(255) NOT NULL,
+		  sys_sunrise INT NOT NULL,
+		  sys_sunset INT NOT NULL,
+		  timezone INT NOT NULL,
+		  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		  PRIMARY KEY (id),
+		  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+		) ENGINE=InnoDB;`
+
+	}
+
 	_, err := db.Exec(query)
 	return err
 }
